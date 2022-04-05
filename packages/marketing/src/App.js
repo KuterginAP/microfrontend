1401d3199bb11afc4be91f8a9e442a6778c86770
Alexey Kutergin <alexey.kutergin@fastdev.se>
2022-04-05 21:05:41 +0400

    fix5

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -7,7 +7,7 @@ import Pricing from './components/Pricing';
 
 export default () => {
   return (
-    <div>
+    <div className='aaa'>
       <StylesProvider>
         <BrowserRouter>
           <Switch>
